1739bfdd734846c907b81785e82eb9a73564d0f1
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-09-29 16:24:51 +1000

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+my_string = input('Город проживания: ')
+print('Количество введённых символов: ',len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
