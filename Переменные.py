9fdea35b2d05e103afbc6821ac0f56e7e5936cfa
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-09-29 14:14:41 +1000

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,4 @@
+ready_hometask = 12
+used_hours = 1.5
+course_name = 'Python'
+print('Курс:',course_name,'всего задач:',ready_hometask,'затрачено часов:',used_hours,'среднее время выполнения',used_hours / ready_hometask,'часа')
