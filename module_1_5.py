65ce7fba6f8550fbf2faa76fe00464620e6c60da
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-10-13 17:52:38 +1000

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, True, "осень"
+print(immutable_var)
+# immutable_var [0] = 2 - Кортеж не имеет свойства изменяться, в отличие от списка []
+mutable_list = [1, True, "осень"]
+mutable_list[0] = 3
+mutable_list[1] = False
+mutable_list[2] = 'лето'
+mutable_list.extend([1, 'зима'])
+print(mutable_list)
