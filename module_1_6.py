346448481dfa08cc4b71b6293458db3a1acba4b1
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-10-14 16:35:44 +1000

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Bob': 2001, 'Mark': 2020, 'Alex': 2010}
+print(my_dict)
+print(my_dict['Bob'])
+print(my_dict.get('Fill'))
+my_dict.update({'Piter': 2003, 'Marina': 2016})
+no_d = my_dict.pop('Mark')
+print(no_d)
+my_set = {12, 'Дом', True, 'Дом', 12}
+print(my_set)
+my_set.add(3)
+my_set.add('home')
+my_set.remove(12)
+print(my_set)
