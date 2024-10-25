f60f33c7b035db1c7117e39cf0dd163b76fd92ae
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-10-25 15:46:21 +1000

    Update module_2.доп.py

--- module_2.доп.py
+++ module_2.доп.py
@@ -1,7 +1,7 @@
 n = int(input('Введите число от 3 до 20, иначе помрете: '))
 result = []
 for i in range(1, 20):
-    for j in range(i+1, 20):
+    for j in range(i + 1, 20):
         if n % (i + j) == 0:
             result.append(i)
             result.append(j)
