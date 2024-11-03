cef74a210a52f1af4cb73aa5794ab83200bd9ad3
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-11-03 19:31:50 +1000

    Update module_3.3.py

--- module_3.3.py
+++ module_3.3.py
@@ -6,7 +6,11 @@ values_list2 = ['cow', True]
 def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
-
+print_params()
+print_params(1,4)
+print_params(False,1,'проверка')
+print_params(b = 25)
+print_params(c = [1,2,3])
 print_params(*values_list)
 print_params(**values_dict)
 print_params(*values_list2, 42)
