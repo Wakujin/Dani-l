83f1f1166fa8a7876c2ae56351b26e4372f00437
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-11-03 19:58:42 +1000

    Update module_3.5.py

--- module_3.5.py
+++ module_3.5.py
@@ -2,7 +2,7 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) == 1:
-        return first
+        return first if str_number != '0' else 1
     else:
         return first * get_multiplied_digits(int(str_number[1:]))
 
