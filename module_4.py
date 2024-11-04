46d7b5cf4d65357b3404d9a1ecdef67a5137b001
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-11-04 11:07:15 +1000

    Add files via upload

---
+++ module_4.py
@@ -0,0 +1,20 @@
+from fake_math import divide as F_d
+from true_math import divide as T_d
+
+print(F_d(2, 0))
+print(T_d(2, 0))
+result1 = F_d(69, 3)
+
+result2 = F_d(3, 0)
+
+result3 = T_d(49, 7)
+
+result4 = T_d(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
