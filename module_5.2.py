b8ab2eb1c02b0a844657e7856229ba285bcff78a
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-11-12 16:33:39 +1000

    Add files via upload

--- module_5.2.py
+++ module_5.2.py
@@ -16,20 +16,83 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
 
 h1 = House('ЖК Эльбрус', 10)
 
 h2 = House('ЖК Акация', 20)
 
-# __str__
-
 print(h1)
 
 print(h2)
 
-# __len__
+print(h1 == h2)  # __eq__
 
-print(len(h1))
+h1 = h1 + 10  # __add__
 
-print(len(h2))
+print(h1)
 
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+
+print(h1)
+
+h2 = 10 + h2  # __radd__
+
+print(h2)
+
+print(h1 > h2)  # __gt__
+
+print(h1 >= h2)  # __ge__
+
+print(h1 < h2)  # __lt__
+
+print(h1 <= h2)  # __le__
+
+print(h1 != h2)  # __ne__
