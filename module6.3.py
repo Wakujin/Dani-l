93782f2cd7fc4ac757abac4c06c547a67cac716c
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-12-02 14:47:34 +1000

    Update module6.3.py

--- module6.3.py
+++ module6.3.py
@@ -41,7 +41,7 @@ class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-        self._cords[2] -= abs(dz) * self.speed / 2
+        self._cords[2] -= abs(dz) * self.speed // 2
 
 
 class PoisonousAnimal(Animal):
@@ -50,6 +50,12 @@ class PoisonousAnimal(Animal):
 
 class Duckbill(PoisonousAnimal, Bird, AquaticAnimal):
     sound = 'Click-click-click'
+
+    def __init__(self, speed):
+        super().__init__(speed)
+
+    def attack(self):
+        super().attack()
 
 
 print(Duckbill.mro())
