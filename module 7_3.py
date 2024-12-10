6e7ed1d9071982cc258c196e9389d2c1a19c5088
Wakujin <97148078+Wakujin@users.noreply.github.com>
2024-12-10 16:08:00 +1000

    Add files via upload

---
+++ module 7_3.py
@@ -0,0 +1,45 @@
+from pstats import count_calls
+
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = list(file_names)
+
+    def get_all_words(self):
+        all_words = {}
+        trash = [',', '.', '=', '!', '?', ';', ':', ' - ']
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words_string = file.read().lower()
+                for t in trash:
+                    words_string = words_string.replace(t, '')
+                words = words_string.split()
+                all_words[file_name] = words
+                return all_words
+
+    def find(self, word):
+        word = word.lower()
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            if word in words:
+                result[file_name] = words.index(word) + 1
+            else:
+                continue
+            return result
+
+    def count(self, word):
+        word = word.lower()
+        all_words = self.get_all_words()
+        result = {}
+        for file_name, words in all_words.items():
+            count = words.count(word)
+        result[file_name] = count
+        return result
+
+
+finder2 = WordsFinder('test_file.txt')
+
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
